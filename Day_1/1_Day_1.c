94029e5cb00b17420740db081548eb2ad1fd2bed
Jatinraheja1 <jatinraheja02235@gmail.com>
2026-05-28 14:18:05 +0530

    1_Day_1

---
+++ Day_1/1_Day_1.c
@@ -0,0 +1,12 @@
+//Write a program to Calculate sum of first N natural numbers. 
+#include<stdio.h>
+int main(){
+    int n, sum=0;
+    printf("enter the number upto which sum is required :");
+    scanf("%d", &n);
+    for(int i=0;i<=n;i++){
+        sum += i;
+    }
+    printf("required sum is : %d", sum);
+    return 0;
+}
