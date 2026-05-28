32161a091163433b919045443205a344b0353a87
Jatinraheja1 <jatinraheja02235@gmail.com>
2026-05-28 14:35:50 +0530

    factorial

---
+++ Day_1/factorial.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n,fact=1;
+    printf("enter the number whose factorial is required :");
+    scanf("%d", &n);
+    for(int i=1;i<=n;i++){
+        fact = fact*i;
+    }
+    printf("Required factorial is : %d",fact);
+    return 0;
+}
