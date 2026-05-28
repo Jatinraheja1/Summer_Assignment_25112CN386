89f8e185273b7e8d27d41e7c001d80abaa4186e0
Jatinraheja1 <jatinraheja02235@gmail.com>
2026-05-28 14:42:28 +0530

    count digits

---
+++ Day_1/count.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int num,count=0;
+    printf("enter the number:");
+    scanf("%d",&num);
+    while(num>0){
+        num = num/10;
+        count ++;
+    }
+    printf("digits in entered number is : %d", count);
+    return 0;
+}
