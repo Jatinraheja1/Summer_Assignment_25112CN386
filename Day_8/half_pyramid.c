b555410b1c55d721370ed59fb71366026da4c425
Jatinraheja1 <jatinraheja02235@gmail.com>
2026-06-04 12:51:14 +0530

    half pyramid

---
+++ Day_8/half_pyramid.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int row,column;
+    printf("Enter the number of rows and columns :");
+    scanf("%d %d",&row,&column);
+    for(int i=1;i<=row;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
