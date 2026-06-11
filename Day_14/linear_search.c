85994a2e718cecfe4e124e2a843b2a6cb3e24193
Jatinraheja1 <jatinraheja02235@gmail.com>
2026-06-11 12:16:23 +0530

    Day_15 completed

--- Day_14/linear_search.c
+++ Day_14/linear_search.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main(){
-    int n,key,found=0,arr[n];
+    int n,key,found=0;
     printf("Enter size: ");
     scanf("%d",&n);
+    int arr[n];
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
