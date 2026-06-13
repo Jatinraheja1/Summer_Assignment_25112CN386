9e6218c36f26fe9f251b9a446de738dd25b5a6ce
Jatinraheja1 <jatinraheja02235@gmail.com>
2026-06-13 10:33:47 +0530

    changes

--- Day_17/union.c
+++ Day_17/union.c
@@ -2,28 +2,28 @@
 int main(){
     int n1, n2;
     printf("Enter size of first array: ");
-    scanf("%d", &n1);
+    scanf("%d",&n1);
     int arr1[n1];
     printf("Enter elements of first array: ");
-    for(int i = 0; i < n1; i++) {
-        scanf("%d", &arr1[i]);
+    for(int i=0;i<n1;i++) {
+        scanf("%d",&arr1[i]);
     }
     printf("Enter size of second array: ");
-    scanf("%d", &n2);
+    scanf("%d",&n2);
     int arr2[n2];
     printf("Enter elements of second array: ");
-    for(int i = 0; i < n2; i++) {
-        scanf("%d", &arr2[i]);
+    for(int i=0;i<n2;i++) {
+        scanf("%d",&arr2[i]);
     }
     int unionArr[n1 + n2];
-    int size = 0;
-    for(int i = 0; i < n1; i++) {
+    int size=0;
+    for(int i=0;i<n1;i++) {
         unionArr[size++] = arr1[i];
     }
-    for(int i = 0; i < n2; i++) {
+    for(int i=0;i<n2;i++) {
         int found = 0;
-        for(int j = 0; j < size; j++) {
-            if(arr2[i] == unionArr[j]) {
+        for(int j=0;j<size;j++) {
+            if(arr2[i]==unionArr[j]) {
                 found = 1;
                 break;
             }
@@ -33,8 +33,8 @@ int main(){
         }
     }
     printf("Union of arrays: ");
-    for(int i = 0; i < size; i++) {
-        printf("%d ", unionArr[i]);
+    for(int i=0;i<size;i++) {
+        printf("%d",unionArr[i]);
     }
     return 0;
 }
